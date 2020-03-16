fe44ea9a432d2d1044ba4844e9ac454662cd21a0
zPz-002 <1215126610@qq.com>
2020-03-16 21:25:12 +0800

    initial update

---
+++ file.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 30 16:38:45 2019
+
+@author: DELL
+"""
+
+import requests
+import pygame
+import tkinter as tk
+from PIL import Image,ImageTk
+from tkinter import ttk
